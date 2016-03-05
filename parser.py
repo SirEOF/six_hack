39092bcc6dac9c3a864b4f345978055afd324aa4
petr-tik <tikilyaynen@gmail.com>
2016-03-05 09:37:26 +0000

    added json action parameter and more examples to parse

--- parser.py
+++ parser.py
@@ -7,35 +7,48 @@ def parser(request):
     # takes a string and returns returns functions with parameters
     
     numbers = re.findall(r'\d+', request)
-    print numbers
-    phrase = request.split(' ')
-    
+    phrase = request.split(' ') 
+    currency = 'GBP'  
+
     for num in numbers:
         if len(num) == 6:
             sort_code = int(num)
         elif len(num) == 8:
             acc_number = int(num)
         else:
+            amount = int(num)
 
     if all(x in phrase for x in ['card']):
         print request
 
 
     # transfer 20 to
-    if any(x in phrase for x in ['send', 'wire', 'transfer']):
-
-        amount = 0
-        currency = 'GBP'         
+    if any(x in phrase for x in ['send', 'wire', 'transfer']):   
+        action = 'transfer'
         if 'to' in phrase:
-            recepient = phrase[phrase.index('to') + 1]
-        else:
-            #  telegram asks who to send to
-            # 
-            pass
-    
-    print json.dumps({"amount" : amount, 
-                        "recepient" : {"name" : recepient, "sort code" : sort_code, "acc_number" : acc_number}, 
+            next = phrase[phrase.index('to') + 1]
+            if not next.isdigit():
+                recepient = next
+            else:
+                recepient = None
+        
+
+    if any(x in phrase for x in ['block', 'lost', 'stole', 'stolen']):
+        action = 'block'
+
+
+    return json.dumps({"action": action,
+                        "amount" : amount, 
+                        "recepient" : {"name" : recepient, 
+                                        "sort code" : sort_code, 
+                                        "acc_number" : acc_number}, 
                         "currency" : currency})
     
 
 parser('send 20 to 23435465 812395')
+parser('send 150 to Bob')
+parser('I\'ve lost my card')
+parser('My card has been stolen')
+parser('let\'s block my card')
+parser('add Bob\'s details: bank_acc: 12345678, sort 123456')
+
