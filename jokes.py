909da81e53275150cedc3d03788f11b4e945a845
petr-tik <tikilyaynen@gmail.com>
2016-03-05 18:43:25 +0000

    jokes source file

---
+++ jokes.py
@@ -0,0 +1,9 @@
+#!/bin/env/python
+
+JOKES = ["I just deleted all the German names off my phone. It's Hans free", 
+"What's the difference between a 'hippo' and a 'Zippo'? One is really heavy, the other is a little lighter", 
+"If I could take just one thing to a desert island I probably wouldn't go", 
+"Jesus fed 5,000 people with two fishes and a loaf of bread. That's not a miracle. That's tapas", 
+"The first time I met my wife, I knew she was a keeper. She was wearing massive gloves", 
+"My Dad used to say 'always fight fire with fire', \
+which is probably why he got thrown out of the fire brigade."]
